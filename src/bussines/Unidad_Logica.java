be583eef7e3eaa152ad0492b2dc9afc58ec22d76
jeremio23 <jeremie16@live.com>
2016-05-17 23:58:19 +0200

    Unidad Logica Acabado

--- src/bussines/Unidad_Logica.java
+++ src/bussines/Unidad_Logica.java
@@ -3,7 +3,16 @@ package bussines;
 import java.util.List;
 import java.util.Map;
 
+import persistence.ActividadDAOImp;
+import persistence.AsignaturaDAOImp;
+import persistence.ClaseDAOImp;
+import persistence.CuatrimestreDAOImp;
 import persistence.DAL;
+import persistence.ExamenClaseDAOImp;
+import persistence.ExamenDAOImp;
+import persistence.ExamenPoliformatDAOImp;
+import persistence.ExamenPracticasDAOImp;
+import persistence.NotificacionDAOImp;
 import persistence.PracticasDAOImp;
 import persistence.UniversidadDAOImp;
 
@@ -90,26 +99,93 @@ public class Unidad_Logica {
 	/*FIN GETTER Y SETTER*/
 
 	/*ACTIVIDAD*/
+	public void eliminarActividad(int id_actividad) {
+	  dal.eliminarActividad(id_actividad);
+		
+	}
+
+	public Actividad crearActividad(Actividad actividad) {
+	   return dal.crearActividad(actividad);
+	}
+
+	public void editarActividad(Actividad actividad) {
+		dal.editarActividad(actividad);
+		
+	}
+
+	public Actividad obtenerInformacionDeActividad(int id_actividad) {
+		return dal.obtenerInformacionDeActividad(id_actividad);
+	}
 	/*FIN ACTIVIDAD*/
+	
 	/*ASIGNATURA*/
 	public Asignatura obtenerInformacionAsignatura(String tituloAsignatura){
 		return dal.obtenerInformacionAsignatura(tituloAsignatura);
 		}
+	
 	public Asignatura crearAsignatura(Asignatura asignatura){
 	return dal.crearAsignatura(asignatura);
 	}
 	
+	public List<Asignatura> obtenerAsignaturasPorCuatrimestre(int cuatrimestre) {
+		return dal.obtenerAsignaturasPorCuatrimestre(cuatrimestre);
+	}
+	
+	public boolean existeAsignatura(String nombreAsignatura) {
+		return dal.existeAsignatura(nombreAsignatura);
+	}
+	
+	public void eliminarAsignatura(int id_asignatura) {
+		dal.eliminarAsignatura(id_asignatura);
+		
+	}
+	
+	public void editarAsignatura(Asignatura asignatura) {
+		dal.editarAsignatura(asignatura);
+		
+	}
 	/*FIN ASIGNATURA*/
+	
 	/*CLASE*/
+	public Clase obtenerInformacionDeClase(int id_clase) {
+		return dal.obtenerInformacionDeClase(id_clase);
+	}
+
+	public void eliminarClase(int id_clase) {
+		 dal.eliminarClase(id_clase);
+		
+	}
+
+	public Clase crearClase(Clase clase) {
+		return dal.crearClase(clase);
+	}
+
+	public void editarClase(Clase clase) {
+		dal.editarClase(clase);
+		
+	}
 	/*FIN CLASE*/
+	
 	/*CUATRIMESTRE*/
 	public Cuatrimestre obtenerInformacionCuatrimestre(int id_cuatrimestre){
 		return dal.obtenerInformacionCuatrimestre(id_cuatrimestre);
 	}
+	
 	public Cuatrimestre crearCuatrimestre(Cuatrimestre cuatrimestre){
 		return dal.crearCuatrimestre(cuatrimestre);
 	}
+
+	public void eliminarCuatrimestre(int id_cuatrimestre) {
+		dal.eliminarCuatrimestre(id_cuatrimestre);
+		
+	}
+	
+	public void editarCuatrimestre(Cuatrimestre cuatrimestre) {
+		dal.editarCuatrimestre(cuatrimestre);
+		
+	}
 	/*FIN CUATRIMESTRE*/
+	
 	/*CURSO*/ 
 	public Curso obtenerInformacionCurso(int id_curso) {
 		return dal.obtenerInformacionCurso(id_curso);
@@ -123,14 +199,87 @@ public class Unidad_Logica {
 		return dal.crearCurso(curso);
 	}
 	/*FIN CURSO*/
+	
 	/*EXAMENCLASE*/
+	public Examen_Clase obtenerInformacionDeExamen_Clase(int id) {
+		return dal.obtenerInformacionDeExamen_Clase(id) ;
+	}
+
+	public void eliminarExamen_Clase(int id){
+		dal.eliminarExamen_Clase(id);
+		
+	}
+
+	public Examen_Clase crearExamen_Clase(Examen_Clase examenclase) {
+		return dal.crearExamen_Clase(examenclase) ;
+	}
+
+	public void editarExamen_Clase(Examen_Clase examenclase) {
+		dal.editarExamen_Clase(examenclase);
+		
+	}
 	/*FIN EXAMENCLASE*/
-	/*EXAMEN*/
+	
+	/*EXAMEN */
+	public Examen obtenerInformacionDeExamen(int id) {
+		return dal.obtenerInformacionDeExamen(id);
+	}
+
+	public void eliminarExamen(int id) {
+		dal.eliminarExamen(id);
+		
+	}
+
+	public Examen crearExamen(Examen examen) {
+	return dal.crearExamen(examen);
+	}
+
+	public void editarExamen(Examen examen) {
+		dal.editarExamen(examen);
+		
+	}
 	/*FIN EXAMEN*/
+	
 	/*EXAMENPOLIFORMAT*/
+	public Examen_Poliformat obtenerInformacionDeExamenPoliformat(int id) {
+		return dal.obtenerInformacionDeExamenPoliformat(id);
+	}
+
+	public void eliminarExamenPoliformat(int id) {
+		dal.eliminarExamenPoliformat(id);
+		
+	}
+
+	public Examen_Poliformat crearExamenPoliformat(Examen_Poliformat ExamenPoliformat) {
+		return dal.crearExamenPoliformat(ExamenPoliformat);
+	}
+
+	public void editarExamenPoliformat(Examen_Poliformat ExamenPoliformat) {
+		dal.editarExamenPoliformat(ExamenPoliformat);
+		
+	}
 	/*FIN EXAMENPOLIFOTMAT*/
+	
 	/*EXAMENPRACTICAS*/
+	public Examen_Practicas obtenerInformacionDeExamen_Practicas(int id) {
+		return dal.obtenerInformacionDeExamen_Practicas(id);
+	}
+	
+	public void eliminarExamen_Practicas(int id) {
+		dal.eliminarExamen_Practicas(id);
+		
+	}
+	
+	public void editarExamen_Practicas(Examen_Practicas examenPracticas) {
+		dal.editarExamen_Practicas(examenPracticas);
+		
+	}
+	
+	public Examen_Practicas crearExamen_Practicas(Examen_Practicas examenPracticas) {
+		return dal.crearExamen_Practicas(examenPracticas);
+	}
 	/*FIN EXAMENPRACTICAS*/
+	
 	/*GRADO*/
 	public Grado obtenerInformacionGrado(int id_grado){
 		return dal.obtenerInformacionGrado(id_grado);
@@ -149,6 +298,22 @@ public class Unidad_Logica {
 	}
 	/*FIN GRADO*/
 	/*NOTIFICACIONES*/
+	public Notificacion obtenerInformacionDeNotificacion(int id_Notificacion) {
+		return dal.obtenerInformacionDeNotificacion(id_Notificacion);
+	}
+
+	public void eliminarNotificacion(int id_Notificacion) {
+		dal.eliminarNotificacion(id_Notificacion);
+		
+	}
+
+	public Notificacion crearNotificacion(Notificacion notificacion) {
+		return dal.crearNotificacion(notificacion);
+	}
+
+	public void editarNotificacion(Notificacion notificacion) {
+	  dal.editarNotificacion(notificacion);
+	}
 	/*FIN NOTIFICACIONES*/
 	/*PRACTICAS*/
 	public Practicas obtenerInformacionDePracticas(int id_Practicas) {
@@ -190,6 +355,7 @@ public class Unidad_Logica {
 		
 	}
 	/*FIN UNIVERSIDAD*/
+	
 	/*USUARIO*/
 	public Usuario crearUsuario(Usuario usuario){
 		this.usuario = usuario;
@@ -216,7 +382,6 @@ public class Unidad_Logica {
 	  return	dal.asociarUniversidadUsuario(usuario, id_universidad);
 		
 	}
-	
 	/*FIN USUARIO*/
 	
 }
